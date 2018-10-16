66348e314c47e3f63082ab61edfacccfff889d7c
SasankG <sasankgraju@gmail.com>
2018-10-15 20:34:16 -0400

    Updates

---
+++ precache-manifest.7116c90605c108b94726e3b9447608ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f051a257d8ea9e9a9f81",
+    "url": "/clicky--game/static/js/runtime~main.f051a257.js"
+  },
+  {
+    "revision": "90e847db19832bc3e3e2",
+    "url": "/clicky--game/static/js/main.90e847db.chunk.js"
+  },
+  {
+    "revision": "84fbebd903b0f67dd38c",
+    "url": "/clicky--game/static/js/1.84fbebd9.chunk.js"
+  },
+  {
+    "revision": "90e847db19832bc3e3e2",
+    "url": "/clicky--game/static/css/main.32e4b354.chunk.css"
+  },
+  {
+    "revision": "859fbf345e78feaa31e238050a0e68a8",
+    "url": "/clicky--game/index.html"
+  }
+];
